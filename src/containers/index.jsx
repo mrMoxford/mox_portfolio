fc2de75edef2851ee126e73c47999d017435d182
Mox <88931273+mrMoxford@users.noreply.github.com>
2023-06-26 16:34:04 +0900

    new

--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -1,5 +1,5 @@
 export { default as Projects } from "./projects/Projects";
 export { default as Footer } from "./footer/Footer";
 export { default as Header } from "./header/Header";
-export { default as AboutMe } from "./about-Me/AboutMe";
+export { default as AboutMe } from "./aboutMe/AboutMe";
 
