5250614a9c9b953602f764523ddbcce37a36e981
Mox <88931273+mrMoxford@users.noreply.github.com>
2023-07-14 06:31:42 +0900

    Update InputField.jsx

--- src/components/inputfield/InputField.jsx
+++ src/components/inputfield/InputField.jsx
@@ -4,7 +4,7 @@ const InputField = ({ label, name, type, value }) => {
       <label className="text-white text-sm mb-4" htmlFor={name}>
         {label}
       </label>
-      <input className="border-b-2 ouline-none text-gray-700 w-full bg-gray-200 h-10 px-2 " type={type} value={value} name={name} required />
+      <input className="border-b-2 ouline-none text-gray-700 w-full bg-gray-200 h-10 px-2 rounded-none" type={type} value={value} name={name} required />
     </div>
   );
 };
