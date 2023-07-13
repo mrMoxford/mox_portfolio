72c76e168a99766e376eced37764653674696dbb
Mox <88931273+mrMoxford@users.noreply.github.com>
2023-07-14 06:21:31 +0900

    Update TextAreaField.jsx
    Took rounding off

--- src/components/text-field/TextAreaField.jsx
+++ src/components/text-field/TextAreaField.jsx
@@ -4,7 +4,7 @@ const TextAreaField = ({ name, label }) => {
       <label className="text-white text-sm" htmlFor={name}>
         {label}
       </label>
-      <textarea name={name} rows="5" required className="w-full border-b-2 outline-none bg-gray-200 text-black rounded"></textarea>
+      <textarea name={name} rows="5" required className="w-full border-b-2 outline-none bg-gray-200 text-black "></textarea>
     </div>
   );
 };
